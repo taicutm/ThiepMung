cd831e28479547ff756ba7a11f8b915a1daa0ce1
DucTai <69153374+taicutm@users.noreply.github.com>
2022-03-07 21:37:47 +0700

    update

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Đoan DeThuong'
+const giftUrl = 'https://www.facebook.com/profile.php?id=100013122991396'
+const eventName = 'Chúc Mừng 8-3'
+const titleCard = 'Tặng Bạn Đoan'
+const contentCard =
+  'Chúc Bae luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bà trong hôm nay và cả những ngày sau'
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thucdoan1.jpg'
+const base64 = ''
+const giftImageBase64 = 'data:image/png;base64, ' + base64
